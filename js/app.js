ca85a5a9460389b74c7b4fb9b593ee1282358e8d
Daniel Schep <dschep@gmail.com>
2013-07-05 22:07:12 -0400

    crap.. i hate tabs

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 angular.module('imgurDownloader', [])
-	.config(['$compileProvider', function ($compileProvider) {
-		$compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|blob):/);
-	}])
-	.config(['$routeProvider', function($routeProvider) {
-		$routeProvider
-			.when('/', {
-				templateUrl: 'partials/landing.html',
-				controller: 'LandingCtrl'
-			})
-			.when('/:albumid', {
-				templateUrl: 'partials/album.html',
-				controller: 'AlbumCtrl'
-			})
-			.otherwise({redirectTo: '/'});
-	}]);
+    .config(['$compileProvider', function ($compileProvider) {
+        $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|blob):/);
+    }])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'partials/landing.html',
+                controller: 'LandingCtrl'
+            })
+            .when('/:albumid', {
+                templateUrl: 'partials/album.html',
+                controller: 'AlbumCtrl'
+            })
+            .otherwise({redirectTo: '/'});
+    }]);
 
